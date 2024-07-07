f31c60753e371c5ca4c946fbe78946cadb420898
Hector Cosgalla <valcos98@hotmail.com>
2024-07-07 16:37:22 -0600

    basic template

---
+++ onepiece_tracker_back/src/main/java/com/valcos98/onepiece_tracker_back/OnepieceTrackerBackApplication.java
@@ -0,0 +1,13 @@
+package com.valcos98.onepiece_tracker_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepieceTrackerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepieceTrackerBackApplication.class, args);
+	}
+
+}
---
+++ onepiece_tracker_back/src/test/java/com/valcos98/onepiece_tracker_back/OnepieceTrackerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valcos98.onepiece_tracker_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnepieceTrackerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
